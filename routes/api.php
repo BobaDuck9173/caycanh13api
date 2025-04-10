930744deea8d17081ea67b593ed6f9cee1789033
BobaDuck9173 <bdc9173@gmail.com>
2025-04-10 18:40:35 +0700

    allow vercel access

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\SanPhamController;
 // Add CORS headers to the /sanpham endpoint
 Route::get('/sanpham', function (Request $request) {
     // Set CORS headers
-    header('Access-Control-Allow-Origin: https://caycanh13-git-main-bobaducks-projects.vercel.app');
+    header('Access-Control-Allow-Origin: https://caycanh13.vercel.app');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
     
@@ -18,7 +18,7 @@ Route::get('/sanpham', function (Request $request) {
 // Handle OPTIONS preflight requests
 Route::options('/sanpham', function() {
     return response('', 200)
-        ->header('Access-Control-Allow-Origin', 'https://caycanh13-git-main-bobaducks-projects.vercel.app')
+        ->header('Access-Control-Allow-Origin', 'https://caycanh13.vercel.app')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 });
