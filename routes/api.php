780d1ef3ea248e609151becc223772db29617129
BobaDuck9173 <bdc9173@gmail.com>
2025-04-10 20:43:27 +0700

    "reset all update"

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SanPhamController;
 
-// Add CORS headers to the /sanpham endpoint
-Route::get('/sanpham', function (Request $request) {
-    // Set CORS headers
-    header('Access-Control-Allow-Origin: https://caycanh13.vercel.app');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
-    
-    // Call the original controller method
-    return app()->call([new SanPhamController(), 'index']);
-});
-
-// Handle OPTIONS preflight requests
-Route::options('/sanpham', function() {
-    return response('', 200)
-        ->header('Access-Control-Allow-Origin', 'https://caycanh13.vercel.app')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
-});
+Route::get('/sanpham', [SanPhamController::class, 'index']);
